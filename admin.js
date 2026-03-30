7cbf2bef252c75126cc214a565f9ac4edd1a69db
cepjapura <claudioeduardo_76@yahoo.com.br>
2026-03-29 21:57:19 -0300

    fix: updated capas to be perfectly cut and pasted into the beautiful baby nursery real room scenario

--- admin.js
+++ admin.js
@@ -167,11 +167,11 @@ document.addEventListener('DOMContentLoaded', () => {
             price: 149.90,
             installments: 3,
             images: [
-                "images/capa_vacinacao_thomas.jpg?v=5",
-                "images/capa_vacinacao_vicente.jpg?v=5",
-                "images/capa_vacinacao_heitor.jpg?v=5",
-                "images/capa_vacinacao_olivia.jpg?v=5",
-                "images/capa_vacinacao_benicio.jpg?v=5"
+                "images/capa_vacinacao_thomas.jpg?v=6",
+                "images/capa_vacinacao_vicente.jpg?v=6",
+                "images/capa_vacinacao_heitor.jpg?v=6",
+                "images/capa_vacinacao_olivia.jpg?v=6",
+                "images/capa_vacinacao_benicio.jpg?v=6"
             ],
             rating: 124,
             stars: 5,
